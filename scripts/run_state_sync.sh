8ed8d9fea04270f9c52ba45e7262e67dff7d6231
Touseef Liaqat <touseefliaqat@gmail.com>
2023-12-07 22:20:38 -0800

    Fix mainnet config for state-sync

--- scripts/run_state_sync.sh
+++ scripts/run_state_sync.sh
@@ -6,7 +6,7 @@ cp -r $PWD/state/mainnet/agoric/data/priv_validator_state.json ./priv_validator_
 rm -rf $PWD/state/mainnet/agoric/data/*
 mv ./priv_validator_state.json $PWD/state/mainnet/agoric/data/priv_validator_state.json
 
-docker run -it \
+docker run \
     -v $PWD/state/mainnet/agoric:/root/agoric:rw \
     -v $PWD/scripts:/scripts:rw \
     --entrypoint '/scripts/state_sync.sh' \
